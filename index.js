e63e265e56e26b24117d210e7459cb06148f000a
eci-ravishankar <ravishankar-sahu@idexx.com>
2022-02-27 10:14:25 +0530

    Test

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const homeRoute = require("./routes/home");
 
 dotenv.config({ path: '.env' });
 
