8d0494beb2943c3ffc56f92eab5c9a44285bbbeb
eci-ravishankar <ravishankar-sahu@idexx.com>
2022-02-28 09:59:17 +0530

    Addest test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let express = require("express");
 let app = express();
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 // const authRoute = require("./routes/auth");
 // const userRoute = require("./routes/users");
@@ -8,45 +8,28 @@ const homeRoute = require("./routes/home");
 const PORT = process.env.PORT;
 
 dotenv.config({ path: '.env' });
+const URI = "mongodb+srv://ravi_sahu:Sahu45a@cluster0.4upeo.mongodb.net/myProfile?retryWrites=true&w=majority";
 
-// mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true})
-//         .then(()=>{
-//             console.log("DM connected successfully....")
-//         });
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+        .then(()=>{
+            console.log("DM connected successfully....")
+        });
 
-// const URI = "mongodb+srv://ravi_sahu:Sahu45a@cluster0.4upeo.mongodb.net/myProfile?retryWrites=true&w=majority";
 
 // mongoose.connect(URI, {
-//    useNewUrlParser: true, 
-//    useUnifiedTopology: true 
-// }, err => {
-//    if(err) throw err;
-//    console.log('Connected to MongoDB!!!')
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//     }, err => {
+//     if(err) throw err;
+//     console.log('Connected to MongoDB!!!')
 // })
 
-// const connectDB = async () => {
-//     try {
-//       console.log(URI);
-//       await mongoose.connect(`${URI}`, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//       });
-//       console.log('MongoDB connected');
-//     } catch (error) {
-//       console.log(error.message);
-//       process.exit(1);
-//     }
-//   };
-
-//   connectDB()
 
 app.use(express.json());
 
 // app.use("/auth", authRoute);
 // app.use("/user", userRoute)
-app.use('/home', homeRoute)
+app.use('/home', homeRoute);
 app.use("/", (req, res)=>{
     res.json({name:"Ravi", city:"Indore"})
 });
