02ac7a82f23f5d14f9213dd8e4f63f12cc55c168
eci-ravishankar <ravishankar-sahu@idexx.com>
2022-02-28 09:45:50 +0530

    Testing routes

--- routes/home.js
+++ routes/home.js
@@ -4,8 +4,14 @@ const cryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
 const res = require("express/lib/response");
 
-router.get("/",(req, rest)=>{
-    res.json("TEsting here....")
+router.get("/",(req, res)=>{
+    res.json([
+        {id:1, name:"A", age:50, city:'Indore'},
+        {id:2, name:"B", age:50, city:'Indore'},
+        {id:3, name:"C", age:50, city:'Indore'},
+        {id:4, name:"C", age:50, city:'Indore'},
+        {id:5, name:"E", age:50, city:'Indore'},
+    ])
 });
 
 module.exports =router;
