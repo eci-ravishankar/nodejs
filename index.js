ef80a8e88fd80df73465a7a85d2dc155e6ce5c3a
eci-ravishankar <ravishankar-sahu@idexx.com>
2022-02-28 09:38:30 +0530

    Testing routes

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 let express = require("express");
 let app = express();
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const authRoute = require("./routes/auth");
-const userRoute = require("./routes/users");
-const homeRoute = require("./routes/home");
+// const authRoute = require("./routes/auth");
+// const userRoute = require("./routes/users");
+// const homeRoute = require("./routes/home");
+const PORT = process.env.PORT;
 
 dotenv.config({ path: '.env' });
 
@@ -15,7 +16,7 @@ dotenv.config({ path: '.env' });
 //             console.log("DM connected successfully....")
 //         });
 
-const URI = "mongodb+srv://ravi_sahu:Sahu45a@cluster0.4upeo.mongodb.net/myProfile?retryWrites=true&w=majority";
+// const URI = "mongodb+srv://ravi_sahu:Sahu45a@cluster0.4upeo.mongodb.net/myProfile?retryWrites=true&w=majority";
 
 // mongoose.connect(URI, {
 //    useNewUrlParser: true, 
@@ -43,12 +44,12 @@ const URI = "mongodb+srv://ravi_sahu:Sahu45a@cluster0.4upeo.mongodb.net/myProfil
 
 app.use(express.json());
 
-app.use("/auth", authRoute);
-app.use("/user", userRoute)
+// app.use("/auth", authRoute);
+// app.use("/user", userRoute)
 app.use("/", (req, res)=>{
     res.json({name:"Ravi", city:"Indore"})
 });
 
-app.listen(8800, ()=>{
-    console.log("server is started at 8080")
+app.listen(process.env.PORT|| 8800, ()=>{
+    console.log("server is started at " , process.env.PORT)
 });
